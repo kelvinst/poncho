6d916bbaf4ead03bc3ba897907a04bdd7ed8a43a
kelvinst <kelvin.stinghen@gmail.com>
2018-09-25 13:45:09 -0300

    Configuring for release

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule Poncho.MixProject do
   use Mix.Project
 
+  @github_url "https://github.com/kelvinst/poncho"
+
   def project do
     [
       app: :poncho,
       version: "0.1.0",
       elixir: "~> 1.7",
+      description: "A toolkit to work with poncho projects",
+      source_url: @github_url,
+      homepage_url: @github_url,
+      package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs()
     ]
   end
 
@@ -18,9 +25,26 @@ defmodule Poncho.MixProject do
     ]
   end
 
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Kelvin Stinghen"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @github_url}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md", "CHANGELOG.md"]
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
